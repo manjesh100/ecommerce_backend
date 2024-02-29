710d8cda090f1c9c11fe11c7eb17f7c0d7bcabaa
IT <manjeshphp@gmail.com>
2024-03-01 01:53:32 +0530

    port change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs= require("fs");
 const path = require("path");
 const express =require('express');
 const app=express();
-const port =3000;
+const port =8000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
