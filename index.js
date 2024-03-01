29624eb823f7ff8c4dbb4304c6cfec75f25cb5e7
IT <manjeshphp@gmail.com>
2024-03-01 23:34:05 +0530

    route log

--- index.js
+++ index.js
@@ -26,17 +26,17 @@ app.get('/api/noida',(req, res)=>{
 
 
      
-const authRouteURL = require('./src/routes/user-routes');
-app.use('/auth', authRouteURL);
+// const authRouteURL = require('./src/routes/user-routes');
+// app.use('/auth', authRouteURL);
 
-const categoryUrl = require('./src/routes/category-routes');
-app.use('/category', categoryUrl);
-const productRoutesUrl = require('./src/routes/product-routes');
-app.use('/product', productRoutesUrl);
+// const categoryUrl = require('./src/routes/category-routes');
+// app.use('/category', categoryUrl);
+// const productRoutesUrl = require('./src/routes/product-routes');
+// app.use('/product', productRoutesUrl);
 
 
-const cartRoutesUrl = require('./src/routes/cart-routes');
-app.use('/cart', cartRoutesUrl);
+// const cartRoutesUrl = require('./src/routes/cart-routes');
+// app.use('/cart', cartRoutesUrl);
 
 
 
