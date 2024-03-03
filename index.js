11709126b51bc1ab2b261d6c93de69bb987d3bb2
IT <manjeshphp@gmail.com>
2024-03-03 23:12:23 +0530

    change

--- index.js
+++ index.js
@@ -24,17 +24,17 @@ app.get('/api/noida',(req, res)=>{
 
 
      
-const authRouteURL = require('./src/routes/user-routes');
-app.use('/api/auth', authRouteURL);
+// const authRouteURL = require('./src/routes/user-routes');
+// app.use('/api/auth', authRouteURL);
 
-const categoryUrl = require('./src/routes/category-routes');
-app.use('/api/category', categoryUrl);
-const productRoutesUrl = require('./src/routes/product-routes');
-app.use('/api/product', productRoutesUrl);
+// const categoryUrl = require('./src/routes/category-routes');
+// app.use('/api/category', categoryUrl);
+// const productRoutesUrl = require('./src/routes/product-routes');
+// app.use('/api/product', productRoutesUrl);
 
 
-const cartRoutesUrl = require('./src/routes/cart-routes');
-app.use('/api/cart', cartRoutesUrl);
+// const cartRoutesUrl = require('./src/routes/cart-routes');
+// app.use('/api/cart', cartRoutesUrl);
 
 
 
