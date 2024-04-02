6100a44e104b9862e602e424ec384888b5a4c79a
IT <manjeshphp@gmail.com>
2024-04-02 23:03:56 +0530

    database add

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const password = 'abhi@9266'; // Replace with your actual password
 const encodedPassword = encodeURIComponent(password);
 //const dbURI = `mongodb+srv://manjeshphp:${encodedPassword}@cluster0.z6bxu7d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const dbURI =`mongodb+srv://manjeshphp:${encodedPassword}@ecommerce.haoxoa7.mongodb.net/?retryWrites=true&w=majority&appName=ecommerce
-`;
-
+const dbURI =`mongodb+srv://manjeshphp:${encodedPassword}@ecommerce.haoxoa7.mongodb.net/?retryWrites=true&w=majority&appName=ecommerce`;
 //IT DATA 
 mongoose.connect(dbURI)
   .then(() => {
